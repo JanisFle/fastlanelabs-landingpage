5438aa6bd1afeb015b7c0fa738c6b6fc451e0a34
Janis Laptop <75389461+JanisFle@users.noreply.github.com>
2024-10-12 14:13:31 +0200

    terms

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3004;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
